700e10c330b1eddca9fda07b8ac3b67abe524c72
b-tush <tush.projects05@gmail.com>
2021-08-22 00:55:32 +0530

    Add files via upload

--- stm32f4xx_drivers/drivers/src/stm32f407xx_gpio_drv.c
+++ stm32f4xx_drivers/drivers/src/stm32f407xx_gpio_drv.c
@@ -10,6 +10,7 @@
 #include<stdio.h>
 #include<stdint.h>
 
+#define PinNo    						pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber
 
 /****************************************************************************************************
  * @fn      		  - GPIO_PeriClockControl
@@ -31,7 +32,7 @@ void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EorD)			/* Pointer to 
 		{
 			if(pGPIOx == GPIOA)
 			{
-				GPIOA_PCLK_EN();
+				GPIOA_PCLK_EN();    					    /* #define GPIOA_PCLK_EN()    ( RCC->AHB1ENR | = (1<<0)) defined in header file */
 			}else if (pGPIOx == GPIOB)
 			{
 				GPIOB_PCLK_EN();
@@ -62,7 +63,7 @@ void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EorD)			/* Pointer to 
 		{
 			if(pGPIOx == GPIOA)
 						{
-							GPIOA_PCLK_DI();
+							GPIOA_PCLK_DI();         		 /* #define GPIOA_PCLK_DI()      ( RCC->AHB1ENR & = ~(1<<0) ) defined in header file */
 						}else if (pGPIOx == GPIOB)
 						{
 							GPIOB_PCLK_DI();
@@ -128,7 +129,7 @@ void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber)
  * 						1. Configure the mode of GPIO pin.
  * 						2. Configure the Speed.
  * 						3. Configure the PuPd settings.
- * 						4. Configure the Optype
+ * 						4. Configure the OutPut type
  * 						5. Configure the Alt functionality.
  *
  * @param[in]         -
@@ -142,6 +143,57 @@ void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber)
 
 void GPIO_Init(GPIO_Handle_t *pGPIOHandle)																/*Parameter to GPIO Handler*/
 {
+	uint32_t temp=0;
+	//1. Configure the mode of GPIO pin.
+
+	if(pGPIOHandle->GPIO_PinConfig.GPIO_PinMode <= GPIO_MODE_ANALOG)
+	{
+		 temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinMode << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber ));       /* In Pin Mode register configuring GPIO pin Modes i.e pin0 takes two bits  0x2=0, for pin1 1x2=2 starts with 2 */
+		 pGPIOHandle->pGPIOx->MODER &= ~(0x3 << PinNo ); 															   /* Before Setting the Bit we need to Clear the Bit to avoid any overwrite*/
+		 pGPIOHandle->pGPIOx->MODER |= temp;   																		   /* transfer the value to MODER register*/
+
+	}
+
+	else
+	{
+		//Interrupt Mode
+	}
+	//2. Configure the Speed.
+	temp =0;
+
+		temp = ( pGPIOHandle->GPIO_PinConfig.GPIO_PinSpeed << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber ));  /* In Pin Speed values */
+		pGPIOHandle->pGPIOx->OSPEEDR &= ~(0x3 << PinNo);
+		pGPIOHandle->pGPIOx->OSPEEDR |= temp; /* transfer the value to OSPEED register*/
+	temp =0;
+
+	//3. Configure the PuPd settings.
+
+		temp = ( pGPIOHandle->GPIO_PinConfig.GPIO_PinPuPdControl << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber ));   /*Each pin takes 2 bits that's why we are multiplying by 2*/
+		pGPIOHandle->pGPIOx->PUPDR &= ~(0x3 << PinNo);
+		pGPIOHandle->pGPIOx->PUPDR |= temp;  /*Instead assignement operator use Bitwise OR for chanfing the particular bit w/o disturbing the rest */
+
+		temp=0;
+
+	//4. Configure the OutPut type
+
+		temp= ( pGPIOHandle->GPIO_PinConfig.GPIO_PinOPType << (pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber ));  /*left shift to 1 bit */
+		pGPIOHandle->pGPIOx->OTYPER &= ~(0x3 << PinNo);
+		pGPIOHandle->pGPIOx->OTYPER |= temp;
+		temp =0;
+
+	//5. Configure the Alt functionality.
+
+		if(pGPIOHandle->GPIO_PinConfig.GPIO_PinAltFunMode == GPIO_MODE_ALTFN)
+		{
+			uint8_t temp1, temp2;
+
+			temp1 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber /8;
+			temp2 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber %8;
+			pGPIOHandle->pGPIOx->AFR[temp1] &= ~( 0xf << (4  * temp2 ));
+			pGPIOHandle->pGPIOx->AFR[temp1] |= (pGPIOHandle->GPIO_PinConfig.GPIO_PinAltFunMode << (4* temp2));
+
+		}
+
 
 }
 
